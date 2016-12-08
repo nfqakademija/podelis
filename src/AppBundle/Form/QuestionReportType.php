57cffdd13569315fc66eafefdbfe89fe03ddb207
eimmar <klakalasas@gmail.com>
2016-12-08 17:01:53 +0200

    fix

--- src/AppBundle/Form/QuestionReportType.php
+++ src/AppBundle/Form/QuestionReportType.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Form;
 
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -11,7 +12,10 @@ class QuestionReportType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('reason', TextType::class);
+        $builder->add('reason', TextType::class)
+        ->add('question', EntityType::class, [
+            'class' => 'AppBundle\Entity\Question'
+        ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
